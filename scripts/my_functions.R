dd9c62147fd1c326e5f264997fcab20b97157a5c
Afreen Alam <aalam1s@semo.edu>
2020-07-21 18:05:30 -0500

    Styled (using lintr).

--- scripts/my_functions.R
+++ scripts/my_functions.R
@@ -5,16 +5,15 @@
 
 ### Standard error function
 
-# Write a function called std_err that calculates standard error of the mean. 
-
+# Write a function called std_err that calculates standard error of the mean.
 # Standard error = standard deviation / sqrt(sample size)
 
-std_err <- function(values, na.rm = FALSE) { 
-  # has 2 arguments, one to accept numeric values and other tells whether to remove NA
+std_err <- function(values, na.rm = FALSE) {
+# has 2 arguments, one to accept numeric values and other that can remove NA
     ifelse(na.rm, # test
          values <- na.omit(values), # if TRUE
          values) # if FALSE
-  s <- sd(values) # calculates standard deviation 
+  s <- sd(values) # calculates standard deviation
   n <- length(values) # finds sample size
   std_err <- s / sqrt(n) # according to formula above
   return(std_err) # returns result
@@ -24,27 +23,32 @@ std_err <- function(values, na.rm = FALSE) {
 
 # Write a function called scaled_mass
 # It calculates the scaled mass index (SMI)
-# The index scales the mass of the bird to the length 
-# of the tarsus (part of the leg) so that birds of different sizes can be compared fairly
+# The index scales the mass of the bird to the
+# length of the tarsus (part of the leg) so that
+# birds of different sizes can be compared fairly
 
 # The equation to calculate SMI for each individual bird (i) is,
-# SMIi=Mi×(Lo/Li)^b
+# SMIi=mi×(lo/li)^b
 
 # Lo is the mean tarsus length of all birds in the sample
 # Mi and Li are the mass and tarsus length of each individual
-# b is the slope estimate of the regression line on the log-transformed mass and log-transformed tarsus length
+# b is the slope estimate of the regression line on
+# the log-transformed mass and log-transformed tarsus length
 
 
 # The function has three arguments
-scaled_mass <- function(mass = 0, # accepts numeric values for mass of birds, default = 0
-                        tarsus = 0, # accepts numeric values for length of tarsus, default = 0
-                        slope = 0) { # Default = 0  
+scaled_mass <- function(mass = 0,
+                        # accepts numeric values for mass of birds, default = 0
+                        tarsus = 0,
+                        # values for length of tarsus, default = 0
+                        slope = 0) {
+                        # Default = 0
 # According to formula above
-  Lo <- mean(tarsus)
-  Mi <- mass
-  Li <- tarsus
+  lo <- mean(tarsus)
+  mi <- mass
+  li <- tarsus
   b <- slope
-  scaled_mass <- Mi * (Lo / Li)^b
+  scaled_mass <- mi * (lo / li)^b
   return(scaled_mass) # Gives result
 }
 
